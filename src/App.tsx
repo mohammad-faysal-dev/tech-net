4bbe4ede5b23639cc12a393c47e541ba677859a6
Faysal2123 <foysal2005n1@gmail.com>
2026-06-19 11:51:08 +0600

    feat: set up Redux Toolkit counter with actions and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "./redux/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/counter/counterSlice";
+
 const App = () => {
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <div
-      className="
-     bg-red-500"
-    >
-      Hello World
+    <div className="flex gap-3">
+      <button
+        onClick={() => dispatch(increment())}
+        className="bolder border-2 p-4 text-green-600"
+      >
+        Increment
+      </button>
+      <div>{count}</div>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="bolder border-2 p-4 text-red-600"
+      >
+        Decrement
+      </button>
+      <button className="bolder border-2 p-4 text-green-600" onClick={() => dispatch(incrementByAmount(5))}>incrementByAmount</button>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import routes from ".";
 
-const root = document.getElementById("root");
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import routes from "./routes";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
       <RouterProvider router={routes} />
-    </React.StrictMode>,
-  );
-}
+    </Provider>
+  </React.StrictMode>,
+);
